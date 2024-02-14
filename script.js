ef90571f55a1936238a308cb24bc0c98a665dc47
灘波竜星 <nambaryusei@nadaharyuuseinoMacBook-Pro.local>
2024-02-14 19:36:40 +0900

    変更内容の説明

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function resetViewport() {
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const y = 5;
-camera.position.set(0, y, 5);
+const y = 8;
+camera.position.set(0, y, 30);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
